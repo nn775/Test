08d387ec2f67b195d86edd0516c851c3523239f1
Nikhil <nn775@nyu.edu>
2015-02-17 10:57:35 -0500

    Change DB Name

--- project/DatabaseConnection.php
+++ project/DatabaseConnection.php
@@ -1,7 +1,7 @@
 <?php
  
-$host = 'gbm-mysql-master.cvbz72fzclwf.us-west-2.rds.amazonaws.com'; // MYSQL database host address
-//$host ='sasap2:3306';
+//$host = 'gbm-mysql-master.cvbz72fzclwf.us-west-2.rds.amazonaws.com'; // MYSQL database host address
+host ='sasap2:3306';
 //$db = 'IBMMakeGoodV2'; // MYSQL database name j4^346ZB%Q~MpGB
 $user = 'exporter'; // Mysql Datbase user
 $pass = 'Neo@Ogilvy'; // Mysql Datbase password
